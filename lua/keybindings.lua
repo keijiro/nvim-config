453195e38a18e0089b249279756a0a95f90e0cd1
Keijiro Takahashi <keijiro@gmail.com>
2023-09-30 22:18:56 +0900

    Remove unused keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -3,13 +3,6 @@ require "helpers/keyboard"
 
 g.mapleader = ' '                                                                 -- Use Space, like key for alternative hotkeys
 
--- LSP {{{
-nm('K', '<cmd>lua vim.lsp.buf.hover()<CR>' )                                      -- Hover object
-nm('ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')                                -- Code actions
-nm('gR', '<cmd>lua vim.lsp.buf.rename()<CR>')                                     -- Rename an object
-nm('gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')                                -- Go to declaration
- -- }}}
-
 -- Telescope {{{
 nm('gd', '<cmd>Telescope lsp_definitions<CR>')                            -- Goto declaration
 nm('<leader>p', '<cmd>Telescope resume<CR>')
@@ -21,11 +14,6 @@ nm('<leader>f', '<cmd>Telescope live_grep<CR>')                                 
 nm('<leader>q', '<cmd>Telescope buffers<CR>')                                    -- Show all buffers
 nm('<leader>a', '<cmd>Telescope<CR>')                                            -- Show all commands
 nm('<leader>t', '<cmd>Telescope lsp_dynamic_workspace_symbols<CR>')              -- Search for dynamic symbols
--- }}}
-
--- Trouble {{{
-nm('<leader>x', '<cmd>TroubleToggle<CR>')                                         -- Show all problems in project (with help of LSP)
-nm('gr', '<cmd>Trouble lsp_references<CR>')                                       -- Show use of object in project
 -- }}}
 
 -- Neo Tree {{{
